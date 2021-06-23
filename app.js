af3137de5a7e4f637fccebd586ac9de2078fcfd8
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-23 18:24:10 +0900

    Executing TypeScript Code with Node.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-let age: number;
-
+"use strict";
+var age;
 age = 30;
-
 console.log(age);
-
 // Alternative node.js with TypeScript :: Not Recommended by Maximilian
 // https://github.com/TypeStrong/ts-node
